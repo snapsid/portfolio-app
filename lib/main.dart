7fdaf8df5b85dec7794f9fcead113a0ade2bbdf0
Rudra Malvankar <rudracmalvankar@gmail.com>
2023-05-27 12:20:52 +0530

    first Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ void main() {
     initialRoute: '/',
     debugShowCheckedModeBanner: false,
     routes: {
-      '/project': (context) => MyProject(),
-      '/': (context) => MyHome(),
-      '/about': (context) => MyApp(),
+      '/project': (context) => const MyProject(),
+      '/': (context) => const MyHome(),
+      '/about': (context) => const MyApp(),
     },
   ));
 }
@@ -33,109 +33,107 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: Colors.transparent,
       ),
       backgroundColor: Colors.black,
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 60),
-              child: ShaderMask(
-                shaderCallback: (rect) {
-                  return LinearGradient(
-                    begin: Alignment.center,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.black, Colors.transparent],
-                  ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-                },
-                blendMode: BlendMode.dstIn,
-                child: Image.asset(
-                  'assests/bnw.png',
-                  height: 400,
-                  fit: BoxFit.contain,
+      body: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 60),
+            child: ShaderMask(
+              shaderCallback: (rect) {
+                return const LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, Colors.transparent],
+                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: Image.asset(
+                'assests/bnw.png',
+                height: 400,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.55),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.55),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Hello I am',
-                    style: TextStyle(color: Colors.white, fontSize: 30),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text('Siddharth Chopra',
-                      style: TextStyle(color: Colors.white, fontSize: 40)),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Software Developer',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text('Hire Me'),
-                      style: TextButton.styleFrom(
-                        primary: Colors.black,
-                        backgroundColor: Colors.white, // Background Color
-                      ),
+                const Text(
+                  'Hello I am',
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text('Rudra Malvankar',
+                    style: TextStyle(color: Colors.white, fontSize: 40)),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Software Developer (Learner)',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Hire Me'),
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white, // Background Color
                     ),
-                    width: 120,
                   ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            FontAwesomeIcons.instagram,
-                            color: Colors.white,
-                          )),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            FontAwesomeIcons.linkedin,
-                            color: Colors.white,
-                          )),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            FontAwesomeIcons.github,
-                            color: Colors.white,
-                          )),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            FontAwesomeIcons.twitter,
-                            color: Colors.white,
-                          )),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            FontAwesomeIcons.facebook,
-                            color: Colors.white,
-                          ))
-                    ],
-                  )
-                ],
-              ),
+                  width: 120,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.instagram,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.linkedin,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.github,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.twitter,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.facebook,
+                          color: Colors.white,
+                        ))
+                  ],
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       // body: Container(
       //   alignment: Alignment.center,
@@ -169,8 +167,7 @@ class _MyAppState extends State<MyApp> {
       //       ),
       //       SizedBox(
       //         height: 10,
-      //       ),
-      //       Text('Siddharth Chopra',
+      //       ),Rudra Malvankar',
       //           style: TextStyle(color: Colors.white, fontSize: 40)),
       //       SizedBox(
       //         height: 10,
--- lib/projects.dart
+++ lib/projects.dart
@@ -10,62 +10,62 @@ class MyProject extends StatefulWidget {
 
 class _MyProjectState extends State<MyProject> {
   projetCard(lang, title, description, star) {
-    return Container(
+    return SizedBox(
       width: MediaQuery.of(context).size.width * 0.9,
       height: 220,
       child: Card(
         child: Container(
-          margin: EdgeInsets.only(left: 20, top: 30, right: 10),
+          margin: const EdgeInsets.only(left: 20, top: 30, right: 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 lang,
-                style: TextStyle(
+                style: const TextStyle(
                   color: Colors.white,
                   fontSize: 18,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
               Text(
                 title,
-                style: TextStyle(
+                style: const TextStyle(
                     color: Colors.white,
                     fontSize: 30,
                     fontWeight: FontWeight.w700),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 3,
               ),
               Text(
                 description,
-                style: TextStyle(color: Colors.white70, fontSize: 16),
+                style: const TextStyle(color: Colors.white70, fontSize: 16),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               Row(
                 children: [
-                  Icon(
+                  const Icon(
                     Icons.star,
                     color: Colors.white70,
                     size: 18,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 4,
                   ),
                   Text(
                     star,
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.white70,
                     ),
                   ),
                   Expanded(child: Container()),
                   IconButton(
                       onPressed: () {},
-                      icon: Icon(
+                      icon: const Icon(
                         FontAwesomeIcons.github,
                         color: Colors.white,
                       )),
@@ -74,7 +74,7 @@ class _MyProjectState extends State<MyProject> {
             ],
           ),
         ),
-        color: Color(0xff262628),
+        color: const Color(0xff262628),
       ),
     );
   }
@@ -85,12 +85,12 @@ class _MyProjectState extends State<MyProject> {
       // backgroundColor: Color(0xff1e1e1e),
       backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Color(0xff252525),
-        title: Text('Projects'),
+        backgroundColor: const Color(0xff252525),
+        title: const Text('Projects'),
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(bottom: 20, top: 10),
+          margin: const EdgeInsets.only(bottom: 20, top: 10),
           alignment: Alignment.center,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
